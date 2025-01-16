06980b4874bc079e9d199323db53a8b74e217e7f
Djordje Bozic <djordjebbozic@gmail.com>
2025-01-16 12:22:36 +0000

    Mod: Now uses pytorch for all internal data structures. Handles conversion of numpy to torch for observations, actions and rewards received from the Environment

--- algos/successor_reprs.py
+++ algos/successor_reprs.py
@@ -1,8 +1,9 @@
 from abc import abstractmethod
 from collections import defaultdict
-from typing import Any, Callable, Dict, List, Optional, Tuple
+from typing import Union, Callable, Dict, List, Optional, Tuple
 from gymnasium import Env
 import numpy as np
+import torch
 from tqdm import tqdm
 from envs.gridworld.gridworld import Gridworld
 from tianshou.data.buffer.base import ReplayBuffer, Batch
@@ -13,9 +14,6 @@ from tianshou.data.buffer.base import ReplayBuffer, Batch
 
 class SFTabular:
     def __init__(self, n_states: int, n_acts: int, step_size: float, disc_fact: float, obs_to_ind: Callable, act_to_ind: Callable) -> None:
-        
-        # These functions are needed to convert observations and actions to their indexes in the Psi table. 
-        # These functions should either be provided by the environment, or written with a specific environment in mind.
         self._obs_to_ind: Callable = obs_to_ind
         self._act_to_ind: Callable = act_to_ind
 
@@ -24,23 +22,17 @@ class SFTabular:
         self.step_size = step_size
         self.disc_fact = disc_fact
 
-        # psi_sparse is a 3D tensor storing all possible psi^\pi(s,a) from (Barreto et al., 2018)
-        # To get psi^\pi(s,a) index psi_sparse[s,a,:]
-        # psi_sparse(s,a) \in R^{|S|^2A}
-        self.psi_sparse = np.zeros((n_states, n_acts, n_states**2 * n_acts))
-        # psi_dense 3D tensor counting all possible transitions. It represents a modified version of 
-        # \Psi from (Machado et al., 2021) that stores which action was taken in s.
-        self.psi_dense = np.zeros((n_states, n_acts, n_states))
-    
-    # Psi from (Machado et al., 2021)
+        self.psi_sparse = torch.zeros((n_states, n_acts, n_states**2 * n_acts))
+        self.psi_dense = torch.zeros((n_states, n_acts, n_states))
+
     @property
     def Psi(self):
-        return np.sum(self.psi_dense, axis=1)
+        return torch.sum(self.psi_dense, dim=1)
 
-    def _transition_to_dense_index(self, obs: np.ndarray, action:np.ndarray, next_obs:np.ndarray) -> Tuple[int]:
+    def _transition_to_dense_index(self, obs: torch.Tensor, action: torch.Tensor, next_obs: torch.Tensor) -> Tuple[int]:
         return self._obs_to_ind(obs), self._act_to_ind(int(action)), self._obs_to_ind(next_obs)
     
-    def _transition_to_one_hot_index(self, obs: np.ndarray, action:np.ndarray, next_obs:np.ndarray) -> int:
+    def _transition_to_one_hot_index(self, obs: torch.Tensor, action: torch.Tensor, next_obs: torch.Tensor) -> int:
         obs_ind, action_ind, next_obs_ind = self._transition_to_dense_index(obs=obs, action=action, next_obs=next_obs)
         return self._dense_index_to_one_hot_index(obs_ind=obs_ind, next_obs_ind=next_obs_ind, action_ind=action_ind)
 
@@ -55,23 +47,41 @@ class SFTabular:
         next_obs_ind = remaining % self.n_states
         return obs_ind, act_ind, next_obs_ind
     
-    def one_hot_index_to_vector(self, one_hot_index:int=None) -> np.ndarray:
-        # This returns one-hot vector \phi(s,a,s') from (Barreto et al., 2018)
-        one_hot_vec = np.zeros(self.n_states**2 * self.n_acts)
+    def one_hot_index_to_vector(self, one_hot_index: int = None) -> torch.Tensor:
+        one_hot_vec = torch.zeros(self.n_states**2 * self.n_acts)
         if one_hot_index is not None:
             one_hot_vec[one_hot_index] = 1
         return one_hot_vec
     
-    def select_action(self, obs: np.ndarray) -> int:
-        return np.random.randint(0, self.n_acts)
+    def select_action(self, obs: torch.Tensor) -> int:
+        return torch.randint(0, self.n_acts, (1,)).item()
     
-    def store_weights(self, path:str) -> Tuple:
-        np.savez(path, psi_sparse=self.psi_dense, psi_dense=self.psi_dense, Psi=self.Psi)
+    def store_weights(self, path: str) -> Tuple:
+        torch.save({'psi_sparse': self.psi_sparse, 'psi_dense': self.psi_dense, 'Psi': self.Psi}, path)
         return 'psi_sparse', 'psi_dense', 'Psi'
     
-    @abstractmethod
-    def store_trnsition(self, obs: np.ndarray, next_obs: np.ndarray, action: np.ndarray, reward: np.ndarray, terminated: bool, truncated: bool) -> None:
-        pass
+    def store_transition(self, obs:Union[torch.Tensor, np.ndarray, np.generic], next_obs:Union[torch.Tensor, np.ndarray, np.generic], action:Union[torch.Tensor, np.ndarray, np.generic], reward:Union[torch.Tensor, np.ndarray, np.generic], terminated: bool, truncated: bool) -> Tuple:
+        if isinstance(obs, np.ndarray):
+            obs = torch.from_numpy(obs)
+        elif isinstance(obs, np.generic):
+            obs = torch.tensor(obs.item())
+
+        if isinstance(next_obs, np.ndarray):
+            next_obs = torch.from_numpy(next_obs)
+        elif isinstance(next_obs, np.generic):
+            next_obs = torch.tensor(next_obs.item())
+
+        if isinstance(action, np.ndarray):
+            action = torch.from_numpy(action)
+        elif isinstance(action, np.generic):
+            action = torch.tensor(action.item())
+
+        if isinstance(reward, np.ndarray):
+            reward = torch.from_numpy(reward)
+        elif isinstance(reward, np.generic):
+            reward = torch.tensor(reward.item())
+        
+        return obs, next_obs, action, reward, terminated, truncated
     
     @abstractmethod
     def update(self) -> None:
@@ -79,14 +89,14 @@ class SFTabular:
 
 
 class SFOffPolicy(SFTabular):
-
     def __init__(self, n_states, n_acts, rb: ReplayBuffer, step_size, disc_fact, obs_to_ind, act_to_ind):
         super().__init__(n_states, n_acts, step_size, disc_fact, obs_to_ind, act_to_ind)
         self.rb: ReplayBuffer = rb
         self.trajectory = []
         self.done = False
     
-    def store_trnsition(self, obs: np.ndarray, next_obs: np.ndarray, action: np.ndarray, reward: np.ndarray, terminated: bool, truncated: bool) -> None:
+    def store_transition(self, obs:Union[torch.Tensor, np.ndarray], next_obs:Union[torch.Tensor, np.ndarray], action:Union[torch.Tensor, np.ndarray], reward:Union[torch.Tensor, np.ndarray], terminated: bool, truncated: bool) -> None:
+        obs, next_obs, action, reward, terminated, truncated = super().store_transition(obs, next_obs, action, reward, terminated, truncated)
         transition = Batch(obs=obs, act=action, obs_next=next_obs, rew=reward, terminated=terminated, truncated=truncated)
         self.rb.add(transition)
     
@@ -103,26 +113,19 @@ class SFOffPolicy(SFTabular):
                 action=transition.act,
                 next_obs=transition.obs_next
             )
-            # TODO: Figure out how psi sparse should look here!
-            # Actions needed to update sparse representation. See (Barreto et al., 2018) Equation (3).
             one_hot_index = self._dense_index_to_one_hot_index(obs_ind=obs_ind, action_ind=action_ind, next_obs_ind=next_obs_ind)
             phi_t = self.one_hot_index_to_vector(one_hot_index=one_hot_index)
-            # TODO: Once you have a real non-random policy replace average operator with proper expectation from Equation (3).
-            expect_pi = np.average(self.psi_sparse[next_obs_ind, :, :])
-            self.psi_sparse[obs_ind, action_ind, :] = phi_t + self.disc_fact*expect_pi
+            expect_pi = torch.mean(self.psi_sparse[next_obs_ind, :, :], dim=0)
+            self.psi_sparse[obs_ind, action_ind, :] = phi_t + self.disc_fact * expect_pi
 
-            # Actions needed to update the dense representation. See (Machado et al., 2021) Equation (8) and Algorithm 1.
             for i in range(self.n_states):
                 td_error = 1 if obs_ind == i else 0
-                # TODO: Once the policy is not random any more implement the expectation under the policy instead of np.average
-                td_error += self.disc_fact*np.average(self.psi_dense[next_obs_ind, :, i]) - self.psi_dense[obs_ind, action_ind, i]
-                self.psi_dense[obs_ind, action_ind, i] += self.step_size*td_error
+                td_error += self.disc_fact * torch.mean(self.psi_dense[next_obs_ind, :, i]) - self.psi_dense[obs_ind, action_ind, i]
+                self.psi_dense[obs_ind, action_ind, i] += self.step_size * td_error
                 
-            i+=1
-            # If the next transition leads to the terminal state, 
-            # we move the buffer index to the first step of the next episode
+            i += 1
             if transition.terminated is True:
-                i+=1
+                i += 1
 
 
 class SFOnPolicy(SFTabular):
@@ -132,7 +135,8 @@ class SFOnPolicy(SFTabular):
         self.done = False
         self.psi_sa_rollouts = defaultdict(list)
     
-    def store_trnsition(self, obs: np.ndarray, next_obs: np.ndarray, action: np.ndarray, reward: np.ndarray, terminated: bool, truncated: bool) -> None:
+    def store_transition(self, obs:Union[torch.Tensor, np.ndarray], next_obs:Union[torch.Tensor, np.ndarray], action:Union[torch.Tensor, np.ndarray], reward:Union[torch.Tensor, np.ndarray], terminated: bool, truncated: bool) -> None:
+        obs, next_obs, action, reward, terminated, truncated = super().store_transition(obs, next_obs, action, reward, terminated, truncated)
         transition = Batch(obs=obs, act=action, obs_next=next_obs, rew=reward, terminated=terminated, truncated=truncated)
         self.trajectory.append(transition)
         if terminated or truncated:
@@ -142,9 +146,7 @@ class SFOnPolicy(SFTabular):
         if self.done is False:
             return
         
-        
-        for t in range(len(self.trajectory)-1, 0, -1):
-
+        for t in range(len(self.trajectory) - 1, 0, -1):
             transition_t = self.trajectory[t]
             obs_t, action_t, next_obs_t = self._transition_to_dense_index(
                 obs=transition_t.obs,
@@ -166,11 +168,11 @@ class SFOnPolicy(SFTabular):
                 # See (Barreto et al., 2018) Equation (3).
                 one_hot_index_i = self._dense_index_to_one_hot_index(obs_ind=obs_i, action_ind=action_i, next_obs_ind=next_obs_i)
                 phi_i = self.one_hot_index_to_vector(one_hot_index=one_hot_index_i)
-                psi_sa += self.disc_fact**(i-t) * phi_i
+                psi_sa += self.disc_fact**(i - t) * phi_i
                 
             self.psi_sa_rollouts[(obs_t, action_t)].append(psi_sa.tolist())
-            psis = np.array(self.psi_sa_rollouts[(obs_t, action_t)])
-            self.psi_sparse[obs_t, action_t, :] = np.average(psis, axis=0)
+            psis = torch.tensor(self.psi_sa_rollouts[(obs_t, action_t)])
+            self.psi_sparse[obs_t, action_t, :] = torch.mean(psis, dim=0)
 
             # Actions needed to update the dense representation. See your notebook.
             for i in range(t, 0, -1):
@@ -180,12 +182,11 @@ class SFOnPolicy(SFTabular):
                     action=transition_i.act,
                     next_obs=transition_i.obs_next
                 )
-                self.psi_dense[obs_t, action_t, next_obs_i] += self.disc_fact**(t-i)
+                self.psi_dense[obs_t, action_t, next_obs_i] += self.disc_fact**(t - i)
 
         # Reset trajectory once update is completed
         self.trajectory = []
         self.done = False
-
 
 def train_loop(env: Env, agent: SFTabular, hparams: Dict) -> None:
     for _ in tqdm(range(hparams['n_episodes'])):
@@ -195,7 +196,7 @@ def train_loop(env: Env, agent: SFTabular, hparams: Dict) -> None:
         while not done:
             action = env.action_space.sample()
             next_obs, reward, terminated, truncated, _ = env.step(action)
-            agent.store_trnsition(
+            agent.store_transition(
                 obs=obs, 
                 next_obs=next_obs,
                 action=action, 
@@ -214,8 +215,8 @@ def train_loop(env: Env, agent: SFTabular, hparams: Dict) -> None:
         if hparams['algo_type'] == 'on_policy':
             agent.update()
 
-def plot_Psi(env: Env, weights_path:str, weights_keys: Tuple, states: Dict, name_prefix:str) -> None:
-    psi_tables = np.load(weights_path)
+def plot_Psi(env: Env, weights_path: str, weights_keys:Tuple, states: Dict, name_prefix: str) -> None:
+    psi_tables = torch.load(weights_path)
     Psi = psi_tables[weights_keys[2]]
 
     for state_id, state_obs in states.items():
@@ -271,7 +272,7 @@ if __name__ == '__main__':
         agent=agent,
         hparams=hparams
     )
-    weights_path = os.path.join(store_path, f'{hparams["algo_type"]}_psi.npz')
+    weights_path = os.path.join(store_path, f'{hparams["algo_type"]}_psi.pt')
     weights_keys = agent.store_weights(path=weights_path)
     
     plot_Psi(
@@ -281,4 +282,3 @@ if __name__ == '__main__':
         states=hparams['states'], 
         name_prefix=f'{hparams["algo_type"]}_state'
     )
-    
