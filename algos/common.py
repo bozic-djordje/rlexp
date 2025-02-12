38ff4f13de99cecd6341f0eb855fac180aee6bbc
Djordje <Djordje>
2025-02-12 15:07:44 +0000

    Mod: store_transition() abstract function moved to the Agent class

--- algos/common.py
+++ algos/common.py
@@ -2,17 +2,44 @@ from abc import abstractmethod
 from gymnasium import Env
 from tqdm import tqdm
 from typing import Dict
+import torch
+import numpy as np
+from typing import Union, Tuple
 
 # TODO: Use this class to define some Agent properties, now useful for type hinting
 class Agent:
-    
-    @abstractmethod
-    def store_transition():
-        pass
+
+    def store_transition(self, obs:Union[torch.Tensor, np.ndarray, np.generic], next_obs:Union[torch.Tensor, np.ndarray, np.generic], action:Union[torch.Tensor, np.ndarray, np.generic], reward:Union[torch.Tensor, np.ndarray, np.generic], terminated: bool, truncated: bool) -> Tuple:
+        if isinstance(obs, np.ndarray):
+            obs = torch.from_numpy(obs)
+        elif isinstance(obs, np.generic):
+            obs = torch.tensor(obs.item())
+
+        if isinstance(next_obs, np.ndarray):
+            next_obs = torch.from_numpy(next_obs)
+        elif isinstance(next_obs, np.generic):
+            next_obs = torch.tensor(next_obs.item())
+
+        if isinstance(action, np.ndarray):
+            action = torch.from_numpy(action)
+        elif isinstance(action, np.generic):
+            action = torch.tensor(action.item())
+
+        if isinstance(reward, np.ndarray):
+            reward = torch.from_numpy(reward)
+        elif isinstance(reward, np.generic):
+            reward = torch.tensor(reward.item())
+        
+        return obs, next_obs, action, reward, terminated, truncated
 
     @abstractmethod
     def update():
         pass
+
+    @abstractmethod
+    def store_weights():
+        pass
+
 
 def train_loop(env: Env, agent: Agent, hparams: Dict) -> None:
     for _ in tqdm(range(hparams['n_episodes'])):
