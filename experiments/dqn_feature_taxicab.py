475d104114f7b90bbe9d67a0f1ca46aa16154ad9
Djordje <Djordje>
2025-06-09 18:08:29 +0100

    Mod: Updated files to be in line with the new config convention

--- experiments/dqn_feature_taxicab.py
+++ experiments/dqn_feature_taxicab.py
@@ -24,6 +24,8 @@ if __name__ == '__main__':
     import yaml
     with open(config_path, 'r') as file:
         hparams = load_yaml(file)
+    exp_hparams = hparams["experiment"]
+    env_hparams = hparams["environment"]
     
     writer = SummaryWriter(store_path)
     logger = TensorboardLogger(writer)
@@ -56,40 +58,42 @@ if __name__ == '__main__':
     ]
 
     train_env = FeatureTaxicab(
-        hparams=hparams,
+        hparams=env_hparams,
         location_features=location_features,
-        store_path=store_path
+        store_path=store_path,
+        easy_mode=env_hparams["easy_mode"] if "easy_mode" in env_hparams else False
     )
 
     test_env = FeatureTaxicab(
-        hparams=hparams,
+        hparams=env_hparams,
         location_features=location_features,
-        store_path=store_path
+        store_path=store_path,
+        easy_mode=env_hparams["easy_mode"] if "easy_mode" in env_hparams else False
     )
     
     nnet = FCActionValue(
         in_dim=train_env.observation_space.shape[0],
         num_actions=int(train_env.action_space.n),
-        h=hparams["hidden_dim"]
+        h=exp_hparams["hidden_dim"]
     )
 
-    optim = torch.optim.Adam(nnet.parameters(), lr=hparams["step_size"])
-    rb = ReplayBuffer(size=hparams['buffer_size'])
+    optim = torch.optim.Adam(nnet.parameters(), lr=exp_hparams["step_size"])
+    rb = ReplayBuffer(size=exp_hparams['buffer_size'])
     
     agent = DQNPolicy(
         model=nnet, 
         optim=optim,
         action_space=train_env.action_space, 
-        discount_factor=hparams["disc_fact"], 
-        target_update_freq=hparams["target_update_steps"]
+        discount_factor=exp_hparams["disc_fact"], 
+        target_update_freq=exp_hparams["target_update_steps"]
     )
 
     train_collector = Collector(agent, train_env, rb, exploration_noise=True)
     test_collector = Collector(agent, test_env, exploration_noise=True)
     
-    n_epochs = hparams["n_epochs"]
-    n_steps = hparams["epoch_steps"]
-    epoch_hook_factory = EpsilonDecayHookFactory(hparams=hparams, max_steps=n_epochs*n_steps, agent=agent, logger=logger)
+    n_epochs = exp_hparams["n_epochs"]
+    n_steps = exp_hparams["epoch_steps"]
+    epoch_hook_factory = EpsilonDecayHookFactory(hparams=exp_hparams, max_steps=n_epochs*n_steps, agent=agent, logger=logger)
     save_hook_factory = SaveHookFactory(save_path=f'{store_path}/best_model.pth')
     
     result = OffpolicyTrainer(
@@ -97,7 +101,7 @@ if __name__ == '__main__':
         train_collector=train_collector,
         test_collector=test_collector,
         max_epoch=n_epochs, step_per_epoch=n_steps, step_per_collect=200,
-        update_per_step=0.25, episode_per_test=100, batch_size=hparams["batch_size"],
+        update_per_step=0.25, episode_per_test=100, batch_size=exp_hparams["batch_size"],
         train_fn=epoch_hook_factory.hook,
         test_fn=lambda epoch, global_step: agent.set_eps(0.05),
         save_best_fn=save_hook_factory.hook,
